13f9f812097da777fe5a270c5f6564f77dd06154
omerboyraz <omer.boyraz@isuzu.com.tr>
2022-12-26 11:54:46 +0300

    MavenTest

--- src/test/java/D05_maven/C03_MavenTest.java
+++ src/test/java/D05_maven/C03_MavenTest.java
@@ -8,7 +8,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
 
 public class C03_MavenTest {
+
     public static void main(String[] args) throws InterruptedException {
+
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
